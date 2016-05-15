320b0df669655b56daa0a0cd6ce6c44b05ad824d
kevin <kevin_qianjiawen@hotmail.com>
2016-05-15 16:09:11 -0400

    revise sensitive data

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -5,20 +5,25 @@ package com.example.android.sunshine.app;
  */
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 
 //package com.example.android.sunshine.app;
 
         import android.os.Bundle;
         import android.support.v4.app.Fragment;
-        import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.LayoutInflater;
         import android.view.Menu;
-        import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.MenuItem;
         import android.view.View;
         import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.ShareActionProvider;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -56,7 +61,13 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+        private String mForecastStr;
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -67,12 +78,41 @@ public class DetailActivity extends ActionBarActivity {
 
             Intent detailActivity = getActivity().getIntent();
             if(detailActivity != null && detailActivity.hasExtra(Intent.EXTRA_TEXT)) {
-                String weatherDetail = detailActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastStr = detailActivity.getStringExtra(Intent.EXTRA_TEXT);
 
                 TextView detailText = (TextView) rootView.findViewById(R.id.deatil_text_view);
-                detailText.setText(weatherDetail);
+                detailText.setText(mForecastStr);
             }
             return rootView;
+        }
+
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
         }
     }
 }
